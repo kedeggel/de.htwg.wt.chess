39234fd10f4d0ed254f20906db9e194ca7b7dfec
Sascha Nunes <sanunes@htwg-konstanz.de>
2017-10-17 15:59:28 +0200

    Added mapping to our own chess html-Page

--- de.htwg.wt.chess/app/controllers/Application.java
+++ de.htwg.wt.chess/app/controllers/Application.java
@@ -4,13 +4,14 @@ import de.htwg.chess.controller.IChessController;
 import de.htwg.chess.controller.impl.ChessController;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.chess;
 
 public class Application extends Controller{
 
     static IChessController controller = new ChessController();
 
     public Result startChess() {
-        return ok("chess controller: " + controller.toString());
+        return ok(chess.render("Welcome to our Chess"));
     }
 
 }
