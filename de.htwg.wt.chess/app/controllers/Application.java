2216e4a23c654e4803efaf9b4d7ff11b5f4df1d1
kedeggel <kedeggel@htwg-konstanz.de>
2017-10-18 14:34:50 +0200

    Added dependencies to lib/

--- de.htwg.wt.chess/app/controllers/Application.java
+++ de.htwg.wt.chess/app/controllers/Application.java
@@ -1,17 +1,17 @@
 package controllers;
 
+import de.htwg.chess.Chess;
 import de.htwg.chess.controller.IChessController;
-import de.htwg.chess.controller.impl.ChessController;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.chess;
 
 public class Application extends Controller{
 
-    static IChessController controller = new ChessController();
+    static IChessController controller = Chess.getInstance().getController();
 
     public Result startChess() {
-        return ok(chess.render("Welcome to our Chess"));
+        return ok(chess.render(controller.printTotalBoard()));
     }
 
 }
