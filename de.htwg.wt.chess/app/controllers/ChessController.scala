a7d54282fbb253f3ab4fd112742fdb958c82cdb1
Sascha Nunes <sanunes@htwg-konstanz.de>
2017-12-27 04:02:13 +0100

    Started to implement ajax-calls to the server (not working yet), added basic functionality for using websockets.

--- de.htwg.wt.chess/app/controllers/ChessController.scala
+++ de.htwg.wt.chess/app/controllers/ChessController.scala
@@ -2,12 +2,16 @@ package controllers
 
 import javax.inject._
 
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.stream.Materializer
 import de.htwg.chess.Chess
 import play.api.mvc._
+import play.api.libs.streams.ActorFlow
 
 @Singleton
-class ChessController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ChessController @Inject()(cc: ControllerComponents) (implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc) {
   val controller = Chess.getInstance.getController
+  val tui = Chess.getInstance().getTui
 
   def chess = Action {
     Ok(views.html.chess(controller))
@@ -16,4 +20,32 @@ class ChessController @Inject()(cc: ControllerComponents) extends AbstractContro
     controller.restart
     Ok(views.html.chess(controller))
   }
+  def command(command: String) = Action {
+    tui.processInputLine(command)
+    Ok(views.html.chess(controller))
+  }
+  def printBoard() = Action {
+    Ok(controller.printBoard())
+  }
+
+
+  def socket = WebSocket.accept[String, String] { request =>
+    ActorFlow.actorRef { out =>
+      println("Connect received")
+      ChessWebSocketActorFactory.create(out)
+    }
+  }
+  object ChessWebSocketActorFactory {
+    def create(out: ActorRef) = {
+      Props(new ChessWebSocketActor(out))
+    }
+  }
+  class ChessWebSocketActor(out: ActorRef) extends Actor {
+    def receive = {
+      case msg: String =>
+        out ! controller.printBoard()
+        println("Sent Message: " + msg.getClass)
+    }
+  }
+
 }
--- de.htwg.wt.chess/public/javascripts/main.js
+++ de.htwg.wt.chess/public/javascripts/main.js
@@ -1,20 +1,94 @@
-
+var firstClick = "";
 
 $(document).ready(function(){
     console.log("Document ready!");
 
-    // this is the output of printTotalBoard() which should be sent by a websocket later.
-    var pieceLocation = "A1: Rook WHITE B1: Knight WHITE C1: Bishop WHITE D1: Queen WHITE E1: King WHITE F1: Bishop WHITE G1: Knight WHITE H1: Rook WHITE A2: Pawn WHITE B2: Pawn WHITE C2: Pawn WHITE D2: Pawn WHITE E2: Pawn WHITE F2: Pawn WHITE G2: Pawn WHITE H2: Pawn WHITE A7: Pawn BLACK B7: Pawn BLACK C7: Pawn BLACK D7: Pawn BLACK E7: Pawn BLACK F7: Pawn BLACK G7: Pawn BLACK H7: Pawn BLACK A8: Rook BLACK B8: Knight BLACK C8: Bishop BLACK D8: Queen BLACK E8: King BLACK F8: Bishop BLACK G8: Knight BLACK H8: Rook BLACK"
-    updateBoard(pieceLocation);
-
-    //overwriting with attr() works. (for later updateBoard()-calls)
-    $("#B2").attr("piece", "QueenBLACK");
+    //websockets not used yet
+    //connectWebSocket();
+    updateBoard();
+    registerFieldbuttonListener();
 });
 
-function updateBoard(board) {
+function updateBoard() {
+    var board = "A1: Rook WHITE B1: Knight WHITE C1: Bishop WHITE D1: Queen WHITE E1: King WHITE F1: Bishop WHITE G1: Knight WHITE H1: Rook WHITE A2: Pawn WHITE B2: Pawn WHITE C2: Pawn WHITE D2: Pawn WHITE E2: Pawn WHITE F2: Pawn WHITE G2: Pawn WHITE H2: Pawn WHITE A7: Pawn BLACK B7: Pawn BLACK C7: Pawn BLACK D7: Pawn BLACK E7: Pawn BLACK F7: Pawn BLACK G7: Pawn BLACK H7: Pawn BLACK A8: Rook BLACK B8: Knight BLACK C8: Bishop BLACK D8: Queen BLACK E8: King BLACK F8: Bishop BLACK G8: Knight BLACK H8: Rook BLACK";
+    // Ajax-call not working yet
+    //var board = getBoardAjax();
+
+    $("button.fieldbutton").attr("piece", "");
     var list = board.split(" ");
     for (var i = 0; i < list.length; i++) {
         list[i] = list[i].substring(0, list[i].length - 1);
         $("#" + list[i]).attr("piece", list[++i] + list[++i]);
     }
+}
+
+function getBoardAjax() {
+    var board = "";
+    $.ajax({
+        type: "GET",
+        url: "/printBoard",
+        done: function(data){
+            alert("ajax returned board: " + data);
+            board = data;
+        },
+        error: function(xhr){
+            alert("Error in getBoardAjax(): " + xhr.status + " " + xhr.statusText);
+        }
+    });
+    return board;
+}
+
+function registerFieldbuttonListener() {
+    $("button.fieldbutton").click(function() {
+        var id = this.getAttribute("id");
+        if (firstClick == "") {
+            firstClick = id;
+            alert("firstClick has been set to: " + firstClick);
+        } else {
+            sendCommandAjax(firstClick + "-" + id);
+            firstClick = "";
+            updateBoard();
+        }
+    });
+}
+
+function sendCommandAjax(command) {
+    $.ajax({
+        type: "GET",
+        url: "/" + command,
+        done: [function(data){
+            alert("ajax command invoked, " + data);
+        }],
+        error: function(xhr){
+            alert("Error in sendCommandAjax(): " + xhr.status + " " + xhr.statusText);
+    }
+    });
+}
+
+
+function connectWebSocket() {
+    var websocket = new WebSocket("ws://localhost:9000/websocket");
+    websocket.setTimeout
+
+    websocket.onopen = function(event) {
+        console.log("Connected to Websocket");
+        websocket.send(JSON.stringify({
+            update: "do it"
+        }));
+    };
+
+    websocket.onclose = function () {
+        console.log('Connection with Websocket Closed!');
+    };
+
+    websocket.onerror = function (error) {
+        console.log('Error in Websocket Occured: ' + error);
+    };
+
+    websocket.onmessage = function (e) {
+        console.log("Connection sent a Message: " + e.data)
+        if (typeof e.data === "string") {
+            updateBoard(e.data);
+        }
+    };
 }
