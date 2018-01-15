bc240ee64f138b8c800be8c14381edcbe9a99180
Sascha Nunes <sanunes@htwg-konstanz.de>
2018-01-15 18:02:58 +0100

    Silhouette working nicely, increased size of navbar.

--- de.htwg.wt.chess/app/controllers/ChessController.scala
+++ de.htwg.wt.chess/app/controllers/ChessController.scala
@@ -1,38 +1,58 @@
 package controllers
 
+
 import javax.inject._
 
-import akka.actor.{ Actor, ActorRef, ActorSystem, Props }
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.stream.Materializer
+import com.mohiva.play.silhouette.api.Silhouette
+import com.mohiva.play.silhouette.api.actions.SecuredRequest
 import de.htwg.chess.Chess
-import play.api.mvc._
+import org.webjars.play.WebJarsUtil
+import play.api.i18n.I18nSupport
 import play.api.libs.streams.ActorFlow
+import play.api.mvc._
+import utils.auth.DefaultEnv
 
-import scala.util.{ Failure, Success, Try }
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
 
 @Singleton
-class ChessController @Inject() (cc: ControllerComponents)(implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc) {
+class ChessController @Inject()(
+                                 components: ControllerComponents,
+                                 silhouette: Silhouette[DefaultEnv]
+                               )(
+                                 implicit
+                                 webJarsUtil: WebJarsUtil,
+                                 assets: AssetsFinder,
+                                 system: ActorSystem,
+                                 mat: Materializer
+                               ) extends AbstractController(components) with I18nSupport {
+
   val controller = Chess.getInstance.getController
   val tui = Chess.getInstance().getTui
 
-  def chess = Action {
-    Ok(views.html.chess(controller))
-  }
-  def newChess = Action {
-    controller.restart
-    Ok(views.html.chess(controller))
-  }
-  def command(command: String) = Action {
-    tui.processInputLine(command)
-    Ok(views.html.chess(controller))
-  }
-  def printBoard = Action {
-    Ok(controller.printBoard())
+
+  def chess = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    Future.successful(Ok(views.html.chess(controller, request.identity)))
   }
 
-  def about = Action {
-    println("ABOUT")
-    Ok(views.html.about())
+  def newChess = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    controller.restart
+    Future.successful(Ok(views.html.chess(controller, request.identity)))
+  }
+
+  def command(command: String) = silhouette.UserAwareAction.async { implicit request =>
+    tui.processInputLine(command)
+    Future.successful(Ok(controller.printBoard()))
+  }
+
+  def printBoard = silhouette.UserAwareAction.async { implicit request =>
+    Future.successful(Ok(controller.printBoard()))
+  }
+
+  def about = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    Future.successful(Ok(views.html.about(request.identity)))
   }
 
   def socket = WebSocket.accept[String, String] { request =>
@@ -41,11 +61,13 @@ class ChessController @Inject() (cc: ControllerComponents)(implicit system: Acto
       ChessWebSocketActorFactory.create(out)
     }
   }
+
   object ChessWebSocketActorFactory {
     def create(out: ActorRef) = {
       Props(new ChessWebSocketActor(out))
     }
   }
+
   class ChessWebSocketActor(out: ActorRef) extends Actor {
     override def receive: Receive = {
       case msg: String =>
